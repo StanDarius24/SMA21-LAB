a733db640cbd9b9cac3efd94d3e37e3718256766
StanDarius24 <standarius24@gmail.com>
2021-11-21 17:43:46 +0200

    lab9

--- app/src/main/java/com/example/model/Payment.kt
+++ app/src/main/java/com/example/model/Payment.kt
@@ -1,8 +1,8 @@
 package com.example.model
 
-data class Payment(
-    var timestamp: String,
-    var cost: Double,
-    var name: String,
-    var type: String
+class Payment(
+    var timestamp: String?,
+    var cost: Double?,
+    var name: String?,
+    var type: String?
 )
